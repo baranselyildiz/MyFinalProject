00c605991dddd63ff48cbaa8aaffeb382fb8ec68
baransel yildiz <baranselyildiz@hotmail.com>
2021-02-10 23:35:36 +0300

    10 Şubat

---
+++ Core/Utilities/Results/IDataResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    // Hem data içerecek hem de mesaj ve ya başarı döndürecek o sebeple IResult inharite ettik.
+    public interface IDataResult<T>:IResult
+    {
+        T Data { get; }
+    }
+}
